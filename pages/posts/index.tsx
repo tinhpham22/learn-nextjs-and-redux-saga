77d344ba8bdc0989b60c2b42414fb717edffcc43
PhamTinh <tinhpv@nal.vn>
2021-11-11 17:36:35 +0700

    nextjs navigation

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
-function PostListPage() {
+import { PostResModel } from '../../models/post/post-res.model';
+import { PostList } from '../../shared/constant/datas';
+import styles from '../../styles/Home.module.css'
+
+const PostListPage = () => {
+  const router = useRouter()
+  const postList: PostResModel[] = [...PostList];
+
+  const goToPostDetail = (postId: PostResModel['id']) => {
+    router.push({
+      pathname: 'posts/[postId]',
+      query: {
+        postId: postId
+      }
+    })
+  }
+
   return (
     <>
-      Hello Post List Page!
-      Nextjs sẽ lấy tên thư mục là tên file để làm router.
+      {
+        postList.map((post) => (
+          <div key={post.id} className={styles.grid}>
+            <div className={styles.card} onClick={() => goToPostDetail(post.id)}>
+              {post.id}. {post.contents}
+            </div>
+          </div>
+        ))
+      }
     </>
   )
 }
---
+++ shared/constant/datas.ts
@@ -0,0 +1,14 @@
+export const PostList = [
+  {
+    id: 1, 
+    contents: 'hihihi',
+  },
+  {
+    id: 2, 
+    contents: 'hahhaa',
+  },
+  {
+    id: 3, 
+    contents: 'huhuhu',
+  }
+]
