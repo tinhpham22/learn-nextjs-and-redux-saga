1f0a3a6e94ebbd9cfb0551216df36bece7973b52
PhamTinh <tinhpv@nal.vn>
2021-11-24 08:41:44 +0700

    parallax scroll

--- pages/landingPage/index.tsx
+++ pages/landingPage/index.tsx
@@ -1,48 +1,55 @@
-import Link from 'next/link';
-import React from 'react';
-
-import styles from '../../styles/Home.module.css';
+import React, { useEffect, useCallback, useState } from 'react';
 
 function LandingPage() {
+  const [reveals, ] = useState(document.querySelectorAll('.parallax'));
+  const windowHeight = window.innerHeight;
+  const revealTop = reveals[1].getBoundingClientRect().top;
+
+  // const reveal = useCallback(() => {
+  //   for (let i = 0; i < reveals.length; i++) {
+  //     const revealPoint = 100;
+  //     if (revealTop < windowHeight - revealPoint) {
+  //       reveals[i].classList.add('active');
+  //     } else {
+  //       reveals[i].classList.remove('.active');
+  //     }
+  //   }
+  // }, [revealTop, reveals, windowHeight])
+
+  useEffect(() => {
+    console.log(reveals);
+    
+
+
+    // window.addEventListener('scroll', reveal);
+  }, [reveals])
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <Link href="/posts">
-              <a>Go to About Page</a>
-            </Link>
-          </div>
+    <section className="landing-page-container">
+      <div className="parallax">
+        <div className="content">
+          Scroll Up and Down this page to see the parallax scrolling effect.
+          This div is just here to enable scrolling.
+          Tip: Try to remove the background-attachment property to remove the scrolling effect.
         </div>
+      </div>
 
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <Link href="/posts">
-              Go to About Page
-            </Link>
-          </div>
+      <div className="parallax">
+        <div className="content">
+          Scroll Up and Down this page to see the parallax scrolling effect.
+          This div is just here to enable scrolling.
+          Tip: Try to remove the background-attachment property to remove the scrolling effect.
         </div>
+      </div>
 
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <Link href="/posts" passHref>
-              <div>
-                Go to About Page
-              </div>
-            </Link>
-          </div>
+      <div className="parallax">
+        <div className="content">
+          Scroll Up and Down this page to see the parallax scrolling effect.
+          This div is just here to enable scrolling.
+          Tip: Try to remove the background-attachment property to remove the scrolling effect.
         </div>
-      </main>
-    </div>
+      </div>
+    </section>
   )
 }
 
