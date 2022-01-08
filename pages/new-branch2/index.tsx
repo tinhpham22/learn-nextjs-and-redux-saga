5f4fb7ca80c5d746482b098f7234b1a994b54934
PhamTinh <tinhpv@nal.vn>
2022-01-08 22:29:55 +0700

    new branch 3

---
+++ pages/new-branch2/index.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function NewBranch2(props) {
+  const [state, setState] = useState('');
+
+  useEffect(() => {
+    return () => {
+
+    }
+  }, []);
+
+  return (
+    <>
+      huhuhuhuhuh
+    </>
+  )
+}
+
+export default NewBranch2;
